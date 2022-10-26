a9b17acd3a5c68c88f76fc388ba7d30da588a86c
T-i-n-o <valentino.heubach@web.de>
2022-10-26 14:01:15 +0200

    Modify main.c for exercise 2

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include "espl_lib.h"
 
-int main(int argc, char* argv[])
+int main()
 {
-    printf("Hello ESPL");
+    int number;
+    int exit = 0;
+    
+    while(exit == 0) {
+        
+        printf("Please enter a number between 0 and 9: ");
+        scanf("%d", &number);
+        printf("You entered the number %s. \n", num_to_words(number));
+        
+    
+        printf("Type 0 if you want to continue this program: ");
+        scanf("%d", &exit);
+    }
+    
+    return 0;
 }
